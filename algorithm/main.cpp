de0a2e47c4f37665fdddbc356b013d20883490c7
pengzhyong <pengzhyong@DESKTOP-7L46O16>
2018-03-30 16:17:48 +0800

    增加前序、中序和后序遍历，采用递归和栈两种方法

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -3,8 +3,19 @@
 using namespace std;
 int main()
 {
-	double nodes[10] = { 5,4,5,1,3,10,1,0,2,6 };
-	BaseTree<double> searchTree(nodes, 10);
-
+	int nodes[10] = { 5,4,5,1,3,10,1,0,2,6 };
+	BaseTree<int> searchTree(nodes, 8);
+	searchTree.preOrder();
+	cout << endl;
+	searchTree.inOrder();
+	cout << endl;
+	searchTree.postOrder();
+	cout << endl << endl;
+	searchTree.preOrderUseStack();
+	cout << endl;
+	searchTree.inOrderUseStack();
+	cout << endl;
+	searchTree.postOrderUseStack();
+	cout << endl;
 	return 0;
 }
--- algorithm/tree.h
+++ algorithm/tree.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+#include <stack>
 template <typename T>
 struct TNode
 {
@@ -14,16 +16,31 @@ class BaseTree
 public:
 	BaseTree() = default;
 	BaseTree(T input[], int sz);
+	~BaseTree();
+	void insertEle(TNode<T>* node, TNode<T>* ele);
+	void preOrder();
+	void inOrder();
+	void postOrder();
+
+	void preOrderUseStack();
+	void inOrderUseStack();
+	void postOrderUseStack();
+
 private:
 	TNode<T>* pRoot;
-	void insertEle(TNode<T>* node, TNode<T>* ele);
+	void preOrderPara(TNode<T>* begNode);//非静态成员不能作为默认实参，不能把pRoot作为默认实参传给preOrder。参数在递归调用时用到
+	void inOrderPara(TNode<T>* begNode);
+	void postOrderPara(TNode<T>* begNode);
 };
 
 template <typename T>
 BaseTree<T>::BaseTree(T input[], int sz)
 {
 	if (sz <= 0 || input == nullptr)
+	{
+		pRoot = nullptr;
 		return;
+	}
 	pRoot = new TNode<T>;
 	pRoot->pLeft = nullptr;
 	pRoot->pRight = nullptr;
@@ -65,5 +82,164 @@ void BaseTree<T>::insertEle(TNode<T>* node, TNode<T>* ele)
 		}
 		else
 			insertEle(node->pRight, ele);
+	}
+}
+
+template <typename T>
+BaseTree<T>::~BaseTree()
+{
+
+}
+
+template <typename T>
+void BaseTree<T>::preOrder()
+{
+	preOrderPara(pRoot);
+}
+
+template <typename T>
+void BaseTree<T>::inOrder()
+{
+	inOrderPara(pRoot);
+}
+
+template <typename T>
+void BaseTree<T>::postOrder()
+{
+	postOrderPara(pRoot);
+}
+
+template <typename T>
+void BaseTree<T>::preOrderPara(TNode<T>* begNode)
+{
+	if (begNode == nullptr)
+		return;
+	std::cout << begNode->value << " ";
+	if (begNode->pLeft != nullptr)
+		preOrderPara(begNode->pLeft);
+	if (begNode->pRight != nullptr)
+		preOrderPara(begNode->pRight);
+}
+
+template <typename T>
+void BaseTree<T>::inOrderPara(TNode<T>* begNode)
+{
+	if (begNode == nullptr)
+		return;
+	if (begNode->pLeft != nullptr)
+		inOrderPara(begNode->pLeft);
+	std::cout << begNode->value << " ";
+	if (begNode->pRight != nullptr)
+		inOrderPara(begNode->pRight);
+}
+
+template <typename T>
+void BaseTree<T>::postOrderPara(TNode<T>* begNode)
+{
+	if (begNode == nullptr)
+		return;
+	if (begNode->pLeft != nullptr)
+		postOrderPara(begNode->pLeft);
+	if (begNode->pRight != nullptr)
+		postOrderPara(begNode->pRight);
+	std::cout << begNode->value << " ";
+}
+
+//====方法1，类似广度优先搜索
+template <typename T>
+void BaseTree<T>::preOrderUseStack()
+{
+	stack<TNode<T>*> nodeStack;
+	nodeStack.push(pRoot);
+	while (!nodeStack.empty())
+	{
+		TNode<T>* left = nodeStack.top()->pLeft;
+		TNode<T>* right = nodeStack.top()->pRight;
+		std::cout << nodeStack.top()->value << " ";
+		nodeStack.pop();
+		if (right != nullptr)
+			nodeStack.push(right);
+		if (left != nullptr)
+			nodeStack.push(left);
+	}
+}
+
+//====方法2，类似深度优先搜索
+//template <typename T>
+//void BaseTree<T>::preOrderUseStack()
+//{
+//	stack<TNode<T>*> nodeStack;
+//	TNode<T>* pCur = pRoot;
+//	while (true)
+//	{
+//		while (pCur != nullptr)
+//		{
+//			std::cout << pCur->value << " ";
+//			nodeStack.push(pCur);
+//			pCur = pCur->pLeft;
+//		}
+//		pCur = nodeStack.top()->pRight;
+//		nodeStack.pop();
+//		if (pCur != nullptr)
+//		{
+//			nodeStack.push(pCur);
+//			std::cout << pCur->value << " ";
+//			pCur = pCur->pLeft;
+//		}
+//		if (nodeStack.empty())
+//			break;
+//	}
+//}
+
+template <typename T>
+void BaseTree<T>::inOrderUseStack()
+{
+	stack<TNode<T>*> nodeStack;
+	TNode<T>* pCur = pRoot->pLeft;
+	nodeStack.push(pRoot);
+	while (!nodeStack.empty())
+	{
+		while (pCur != nullptr)
+		{
+			nodeStack.push(pCur);
+			pCur = pCur->pLeft;
+		}
+		pCur = nodeStack.top();
+		std::cout << pCur->value << " ";
+		nodeStack.pop();//已经访问过的节点一定要弹出，以免形成死循环
+		pCur = pCur->pRight;
+		if (pCur != nullptr)
+		{
+			nodeStack.push(pCur);
+			pCur = pCur->pLeft;
+		}
+	}
+}
+
+template <typename T>
+void BaseTree<T>::postOrderUseStack()
+{
+	stack<TNode<T>*> nodeStack;
+	TNode<T>* pCur = pRoot->pLeft;
+	TNode<T>* pPre = nullptr;
+
+	nodeStack.push(pRoot);
+	while (!nodeStack.empty())
+	{
+		pCur = nodeStack.top();
+		if ((pCur->pLeft == nullptr && pCur->pRight == nullptr) || 
+			(pPre != nullptr && (pPre == pCur->pLeft || pPre == pCur->pRight)))
+		{
+			std::cout << pCur->value << " ";
+			pPre = pCur;
+			nodeStack.pop();
+		}
+		else
+		{
+			if (pCur->pRight != nullptr)
+				nodeStack.push(pCur->pRight);
+			if (pCur->pLeft != nullptr)
+				nodeStack.push(pCur->pLeft);
+		}
 	}
 }
